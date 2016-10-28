3e57506347281eb64a3438e7e46d5a9f786af1b4
wzw <wzw@disanbo.com>
2016-10-28 19:40:17 +0800

    tendermint build

--- DOCKER/run.sh
+++ DOCKER/run.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+tendermint node --seeds="$TMSEEDS" --moniker="$TMNAME"
+rt TMREPO=github.com/tendermint/tendermint
+export TMHEAD=master
 mkdir -p $GOPATH/src/$TMREPO
 cd $GOPATH/src/$TMREPO
 git clone https://$TMREPO.git .
@@ -7,4 +10,7 @@ git fetch
 git reset --hard $TMHEAD
 go get -d $TMREPO/cmd/tendermint
 make
-tendermint node --seeds="$TMSEEDS" --moniker="$TMNAME"
+#build test application
+go get github.com/tendermint/tmsp/cmd/dummy
+go get github.com/tendermint/merkleeyes/cmd/merkleeyes
+go get github.com/tendermint/tmsp/cmd/...
